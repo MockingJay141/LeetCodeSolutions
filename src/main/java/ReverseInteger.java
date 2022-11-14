1947eb34a4225866cd68d9ba14cf5ce93b10cb74
Omkar_Kulkarni <omkar14199@gmail.com>
2022-11-14 12:38:47 -0500

    Solution for Reversing the integer using standard method

---
+++ src/main/java/ReverseInteger.java
@@ -0,0 +1,65 @@
+/**
+ * 7. Reverse Integer
+ * Medium
+  * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ */
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        
+        int negative = x;
+        if(x < 0){
+            x = x*(-1);
+        }
+
+        if(x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE){
+            return 0;
+        }
+
+        long reverse = 0L;
+        while (x > 0){long digits = x % 10;
+            reverse = reverse*10 + digits;
+
+            x = x/10;
+
+            if (reverse >= Integer.MAX_VALUE){
+                return 0;
+            }
+        }
+
+
+        if (negative < 0){
+            reverse = reverse * (-1);
+        }
+        return (int) reverse;
+    }
+
+    public static void main(String[] args) {
+        ReverseInteger reverseInteger = new ReverseInteger();
+
+        System.out.println(reverseInteger.reverse(10));
+        System.out.println(reverseInteger.reverse(Integer.MAX_VALUE));
+        System.out.println(reverseInteger.reverse(Integer.MIN_VALUE));
+        System.out.println(reverseInteger.reverse(2345455));
+
+
+    }
+}
